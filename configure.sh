e021e7125a28edcf01be1c4bc23c3ff25b865b3d
nzmehuc <108128347+nzmehuc@users.noreply.github.com>
2022-08-01 07:57:38 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -32,7 +32,7 @@ cat << EOF > /usr/local/etc/xray/config.json
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/"
+          "path": "/cdsu65876jyyfgf"
         }
       }
     }
